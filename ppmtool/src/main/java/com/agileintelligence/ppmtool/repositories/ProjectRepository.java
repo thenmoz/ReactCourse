a3d3c62bd0d06a7ba0efbd1cde5cfc8c79fdc131
NMOz <mozaakub@gmail.com>
2020-05-20 22:46:57 +0700

    FullStack Lecture10

---
+++ ppmtool/src/main/java/com/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.agileintelligence.ppmtool.repositories;
+
+import com.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+    
+}
