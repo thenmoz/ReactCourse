c1a4bc77ea327d6f7b189b287058ef2594aafbbd
NMOz <mozaakub@gmail.com>
2020-05-26 20:56:27 +0700

    FullStack Lecture11

--- ppmtool/src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/agileintelligence/ppmtool/web/ProjectController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
@@ -18,7 +20,7 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project){
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 
